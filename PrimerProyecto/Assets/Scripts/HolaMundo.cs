c7c227bf139d9728f5a317ae55e461181d259dd1
Oscar Eduardo Gonzalez Anaya <osgonan.25@hotmail.com>
2022-02-22 19:54:30 -0800

    hola mundo
    se creo archivo hola mundo

---
+++ PrimerProyecto/Assets/Scripts/HolaMundo.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HolaMundo : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("algo paso");
+        Debug.Log("algo paso");
+        Debug.LogWarning("warning");
+        Debug.LogError("Error");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
