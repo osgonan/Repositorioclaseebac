1e82580b0fda8ddd11b4c8c265273a7b3c54817e
Oscar Eduardo Gonzalez Anaya <osgonan.25@hotmail.com>
2022-03-01 18:40:53 -0800

    Practica Modulo 7
    Implementacion ejercicio modulo 7

--- PrimerProyecto/Assets/Modulo 6/Script/CajaScript.cs
+++ PrimerProyecto/Assets/Modulo 6/Script/CajaScript.cs
@@ -5,18 +5,26 @@ using UnityEngine;
 public class CajaScript : MonoBehaviour
 {
     public GameObject caja;
+    public bool esVerdadero;
     void Awake()
     {
+        esVerdadero = true;
+        //Color c = new Color(Random.value, Random.value, Random.value);
+        //caja.GetComponent<MeshRenderer>().material.color = c;
 
-        Color c = new Color(Random.value, Random.value, Random.value);
-        caja.GetComponent<MeshRenderer>().material.color = c;
-        
     }
- 
+
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        if (esVerdadero)
+        {
+            caja.GetComponent<MeshRenderer>().material.color = Color.white;
+        }
+        else {
+            caja.GetComponent<MeshRenderer>().material.color = Color.black;
+        }
+        esVerdadero = !esVerdadero;
     }
 }
