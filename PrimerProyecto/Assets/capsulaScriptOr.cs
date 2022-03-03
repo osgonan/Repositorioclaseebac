17d178f5da70fe254a8f65a4683f4c48ce87bf90
Oscar Eduardo Gonzalez Anaya <osgonan.25@hotmail.com>
2022-03-02 19:57:05 -0800

    Correcciones
    Se hicieron las correcciones de las observaciones  de la evaluacion

--- PrimerProyecto/Assets/capsulaScriptOr.cs
+++ PrimerProyecto/Assets/capsulaScriptOr.cs
@@ -11,14 +11,14 @@ public class capsulaScriptOr : MonoBehaviour
 
     void FixedUpdate()
     {
-        bool cajaEsVerdadero = caja.GetComponent<CajaScript>().esVerdadero;
-        bool esferaEsVerdadero = esfera.GetComponent<EsferaScript>().esVerdadero;
         //  Color c = new Color(Random.value, Random.value, Random.value);
         // capsula.GetComponent<MeshRenderer>().material.color = c;
+        bool cajaEsVerdadero = caja.GetComponent<CajaScript>().esVerdadero;
+        bool esferaEsVerdadero = esfera.GetComponent<EsferaScript>().esVerdadero;
+
         if (cajaEsVerdadero || esferaEsVerdadero)
         {
             capsula.GetComponent<MeshRenderer>().material.color = Color.white;
-            Debug.Log("Entro");
         }
         else
         {
